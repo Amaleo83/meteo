a9d0b1856f355ea85b798be4886f0adb2a43d6a9
colletq <colletq@outlook.fr>
2024-08-29 17:42:21 +0200

    code node JS & front

--- index.js
+++ index.js
@@ -1,26 +1,56 @@
 const express = require('express');
+const ejs = require('ejs');
 const path = require('path');
+require('dotenv').config()
+const XMLHttpRequest = require('xhr2');
+
 const app = express();
 const port = 8080;
 
+const apiKey = process.env.API_KEY;
+const countrydata = require("./data/country_data.json")
+
 // Middleware pour servir les fichiers statiques (HTML, CSS, JS)
-app.use(express.static('public'));
-
-// Route pour l'API de chatbot
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
-app.post('/api/chatbot', (req, res) => {
-    const userMessage = req.body.message.toLowerCase();
 
-    let botResponse = "Je n'ai pas compris !";
+app.set('views', __dirname+'/views')
+app.set('view engine', 'ejs');
 
-    if (userMessage.includes("salut") || userMessage.includes("bonjour")) {
-        botResponse = "Très bien et toi ?";
-    } else if (userMessage.includes("comment ça va") || userMessage.includes("ça va")) {
-        botResponse = "Je vais bien, merci !";
-    }
+app.get('/', (req,res) => {
+    res.redirect("/meteo")
+})
 
-    res.json({ response: botResponse });
+app.get('/meteo', (req, res) => {
+    res.render('meteo', {countrydata: JSON.stringify(countrydata), port:port, weatherdata: 'null'});
 });
+
+app.get('/meteo/:coords', (req, res) => {
+    let latAndLong = req.params.coords.split(";")
+    let weatherdata;
+                        
+    let xhr = new XMLHttpRequest;
+    xhr.open('GET', 
+    'https://api.tomorrow.io/v4/weather/forecast?location='+latAndLong[0]+','+latAndLong[1]+'&apikey='+apiKey, 
+    true);
+    xhr.onreadystatechange = () => {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            weatherdata = xhr.responseText;
+        }
+    };
+    xhr.send();
+    xhr.onload = () => {
+        weatherDataToday = []
+        weatherDataTomorrow = []
+
+        weatherDataToday = weatherdata
+        weatherDataTomorrow = weatherdata
+
+        res.render('meteo', {countrydata: JSON.stringify(countrydata), port: port, weatherdata: weatherdata})
+    }
+    
+}) 
+
 
 // Démarrer le serveur
 app.listen(port, () => {
